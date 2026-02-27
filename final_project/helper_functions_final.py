077bfbc628079620bc5a1cca2f51ad3532f878dd
Emilio Fromm <81587142+emilio-fromm@users.noreply.github.com>
2026-02-27 21:29:03 +0100

    Refactor file access and update print statements
    Refactored file handling to use relative paths and updated print statements for clarity.

--- final_project/helper_functions_final.py
+++ final_project/helper_functions_final.py
@@ -1,45 +1,56 @@
-# Copyright by Emilio
+# final project, written and copyright by Emilio Fromm
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import json
 import os
+from pathlib import Path
 
+
+our_dir = Path(__file__).parent
+
+# Access a file in the same folder
+file_path = our_dir / "myfile.txt"
 def load_tasks():
+    
     tasks = []
-    if os.path.exists("/Users/emiliofromm/Documents/GitHub/b1-programing/final_project/tasks.txt"):
-        print("File found, loading tasks...")
-
-        with open("/Users/emiliofromm/Documents/GitHub/b1-programing/final_project/tasks.txt", "r") as file:
+    if os.path.exists(our_dir/"tasks.txt"):
+        print("found")
+        
+        with open(our_dir/"tasks.txt", "r") as file:
             for line in file:
                 tasks.append(line.strip())
-            return tasks
-
+            return tasks    
+        
     else:
-        print("File not found!")
+        print("File not found")
         raise HTTPException(status_code=404, detail="File not found")
+        
+    
 
 
 def save_task(task):
     tasks = []
-    print("SAVING TASK: ", task)
+    print("DAS IST DIE TASK: ", task)
     tasks = load_tasks()
     tasks.append(json.dumps(task.__dict__))
-
-    if os.path.exists("/Users/emiliofromm/Documents/GitHub/b1-programing/final_project/tasks.txt"):
-        with open("/Users/emiliofromm/Documents/GitHub/b1-programing/final_project/tasks.txt", "w") as file:
+    
+     
+    if os.path.exists(our_dir/"tasks.txt"):
+        with open(our_dir/"tasks.txt", "w") as file:
             for currentTask in tasks:
-                file.write((currentTask) + "\n")
+                file.write( (currentTask) + "\n")
     else:
-        print("File not found!")
-        raise HTTPException(status_code=404, detail="File not found")
-
+        print("File not found")
+        raise HTTPException(status_code=404, detail="File not found") 
+  
 def save_tasks(tasks):
-
-    if os.path.exists("/Users/emiliofromm/Documents/GitHub/b1-programing/final_project/tasks.txt"):
-        with open("/Users/emiliofromm/Documents/GitHub/b1-programing/final_project/tasks.txt", "w") as file:
+    
+    if os.path.exists(our_dir/"tasks.txt"):
+        with open(our_dir/"tasks.txt", "w") as file:
             for currentTask in tasks:
                 file.write(currentTask + "\n")
     else:
-        print("File not found!")
+        print("File not found")
         raise HTTPException(status_code=404, detail="File not found")
+  
